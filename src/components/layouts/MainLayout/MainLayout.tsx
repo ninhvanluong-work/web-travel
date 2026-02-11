8e5e0fa21f0d29a328494325b0bb8d7d5efb9b34
longtranvan <longtranvan15091999@gmail.com>
2026-02-11 10:49:16 +0700

    fix: layout

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -7,29 +7,9 @@ interface Props {
   children: ReactNode;
 }
 const MainLayout: FCC<Props> = ({ children }) => {
-  React.useEffect(() => {
-    let lastWidth = window.innerWidth;
-
-    const setAppHeight = () => {
-      const doc = document.documentElement;
-      doc.style.setProperty('--app-height', `${window.innerHeight}px`);
-    };
-
-    const handleResize = () => {
-      if (window.innerWidth !== lastWidth) {
-        lastWidth = window.innerWidth;
-        setAppHeight();
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-    setAppHeight(); // Initial set
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
   return (
     <div className="bg-slate-900 flex justify-center items-center min-h-screen">
-      <div className="container relative h-[100dvh] h-[var(--app-height)] min-[768px]:h-[850px] bg-white shadow-2xl overflow-hidden pointer-events-auto">
+      <div className="container relative h-[100vh] supports-[height:100dvh]:h-[100dvh] min-[768px]:h-[850px] bg-white shadow-2xl overflow-hidden pointer-events-auto">
         <main className="h-full overflow-y-auto scrollbar-hide">{children}</main>
       </div>
     </div>
