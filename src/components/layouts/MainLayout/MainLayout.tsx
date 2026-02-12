88f61175638d426bf372ffbadd0e2969dba78b64
longtranvan <longtranvan15091999@gmail.com>
2026-02-12 15:32:58 +0700

    fix: ios

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,16 +1,25 @@
 import type { ReactNode } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 
 import type { FCC } from '@/types';
 
 interface Props {
   children: ReactNode;
 }
+
 const MainLayout: FCC<Props> = ({ children }) => {
+  const [isKeyboardMode, setIsKeyboardMode] = useState(false);
+
   return (
-    <div className="bg-slate-900 flex justify-center items-center min-h-screen">
-      <div className="container relative h-[100vh] supports-[height:100dvh]:h-[100dvh] min-[768px]:h-[850px] bg-white shadow-2xl overflow-hidden pointer-events-auto">
-        <main className="h-full overflow-hidden scrollbar-hide">{children}</main>
+    <div className="bg-slate-900 flex justify-center items-center min-h-dvh">
+      <div className="container relative h-dvh min-[768px]:h-[850px] bg-white shadow-2xl overflow-hidden pointer-events-auto">
+        <main
+          className={`h-full scrollbar-hide ${isKeyboardMode ? 'overflow-auto' : 'overflow-hidden'}`}
+          onFocusCapture={() => setIsKeyboardMode(true)}
+          onBlurCapture={() => setIsKeyboardMode(false)}
+        >
+          {children}
+        </main>
       </div>
     </div>
   );
