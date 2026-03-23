1d15a76c98ceea3dacf0df5990e468b468aa6d43
longtranvan <longtranvan15091999@gmail.com>
2026-03-23 07:08:28 +0700

    feat: new branch develop

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next';
 const ProductPage: NextPage = () => {
   return (
     <div className="flex h-full w-full items-center justify-center bg-black">
-      <p className="text-white/60 text-sm font-dinpro">Sắp ra mắt</p>
+      <p className="text-white/60 text-sm font-dinpro">Sắp mắt</p>
     </div>
   );
 };
